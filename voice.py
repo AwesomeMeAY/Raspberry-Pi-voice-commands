278f8e46c66432e9b2a67a27baedd106261f33cc
AwesomeMeAY <awesomemeandawesomeyou@gmail.com>
2019-12-16 20:44:20 -0600

    Added feature to add websites to search in

--- voice.py
+++ voice.py
@@ -7,23 +7,24 @@ from difflib import get_close_matches
 
 # defining functions that are used 
 def reload_files():
+    """ Order is: files, directories,then paths."""
     found_files = []
     found_dirs = []
-    found_subs = []
-    for sub_dirs, dirs, files in os.walk(os.getcwd()):
+    found_paths = []
+    for paths, dirs, files in os.walk(os.getcwd()):
         found_files += files
         found_dirs += dirs
-        found_subs += sub_dirs
-    return found_files, found_dirs, found_subs
+        found_paths += paths
+    return found_files, found_dirs, found_paths
 
 def find_path(file):
-    for subdir, dirs, files in os.walk(os.getcwd()):
+    for path, dirs, files in os.walk(os.getcwd()):
             for f in files:
                 if f == file:
-                    return os.path.join(subdir, file)
+                    return os.path.join(path, file)
             for d in dirs:
                 if d == file:
-                    return os.path.join(subdir, file)
+                    return os.path.join(path, file)
 
 def file_search(file):
     return get_close_matches(file, current_files)[0]
@@ -38,7 +39,8 @@ def dir_search(directory):
                 dir_list.append("{}\{}".format(requested_dir, dirs))       
         if len(get_close_matches(directory, dir_list)) == 0:
             return find_path(requested_dir)
-        return get_close_matches(directory, dir_list)[0]                           
+        else:
+            return get_close_matches(directory, dir_list)[0]                           
     except:
         print('Could not find directory "{}"!'.format(directory))
 
@@ -46,28 +48,31 @@ def exe(command):
     duckurl = "https://www.duckduckgo.com/?q="
     command = command.lower()
     order = command.split()[0]
-    if "search youtube" in command:
-        # search youtube for command
-        # Starts at index 2 because "search" is index 1 and "youtube" is index 1
-        webbrowser.open(duckurl+ "!you " +command[command.index(command.split()[2]):])
-        return True
+    if order == "search":
+        website = command.split()[1]
+        # if command.split()[1] in websites then it will search the website
+        # instead of googling it.
+        if website in websites:
+            webbrowser.open(duckurl + websites[website] + " " + command[command.index(command.split()[2]):])
+            return True
+        else:
+            webbrowser.open(duckurl + command[command.index(command.split()[1]):])
+            return True
     elif "open directory" in command:
-        # This will open the folder where a specified peice of media is stored
         combined_dir =  dir_search(command[command.index(command.split()[2]):])
         for sub_dir, dirs, files in os.walk(os.getcwd()):
             if combined_dir in sub_dir:
                 subprocess.call(["xdg-open",sub_dir])
                 return True
-
-    elif order == "search":
-        # search duck duck go
-        duckurl = duckurl + command[command.index(command.split()[1]):]
-        webbrowser.open(duckurl)
-        return True
     elif order == "play":
         #this will play movies
         file = file_search(command[command.index(command.split()[1]):])
         subprocess.call(["xdg-open",find_path(file)])
+        return True
+    
+    elif order == "add":
+        # The second letter will be the webbsite third will be the bang
+        websites[command.split()[1]] = command.split()[2]
         return True
     elif order == "refresh" or order == "reload":
         print("{}ing files...".format(order.capitalize()))
@@ -78,14 +83,15 @@ def exe(command):
     else:
         print('Could not recognize command "{}"!'.format(command))
         return False
+
 # speech recognition
 def listening():
     with sr.Microphone() as sauce:
         print("Listening...")
         audio = r.listen(sauce)
     print("recognizing")
-    work = exe(r.recognize_google(audio))
-    if not work:
+    command_worked = exe(r.recognize_google(audio))
+    if not command_worked:
         print("Something went wrong!")
         return False
     else:
@@ -93,10 +99,11 @@ def listening():
 # Constants
 current_files, current_dirs, current_subs = reload_files()
 r = sr.Recognizer()
+websites = {"youtube":"!you"}
 commands = ["open directory We need to deeper", "open directory Kirby's Epic Yarn", "open directory Classical music", "open directory sponge bob",
             "open directory Super mario odyssey ", "open directory batman animated", "search youtube ethoslab", "play the chrwistman tree",
             "search someting", "play spiderman", "reload", "refresh"]
 
 while True:
-    listening()
+    listening()    
     
