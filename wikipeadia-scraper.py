aca0baacdbdad4a908ace2b2f985bc204d596455
awesomemeay <awesomemeandawesomeyou@gmail.com>
2020-08-08 11:01:44 -0500

    Working on a wikipedia reader

---
+++ wikipeadia-scraper.py
@@ -0,0 +1,13 @@
+#!/bin/python
+import requests as Requests
+from sys import argv
+from bs4 import BeautifulSoup as Soup
+
+def wikipeadia_search(article):
+    article = article.replace(" ", "_")
+    request = Requests.get(f'https://en.wikipedia.org/wiki/{article}')
+    print(request.status_code)
+    parser = Soup(request.text, 'html.parser')
+    print(parser.find_all('p')[1].get_text())
+wikipeadia_search("")
+print("succsess")
